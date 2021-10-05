7410ad67a5b1918852c3c11c5f271315b679336a
Mehmet Akif Inan <72706177+Mehmet-akif-inan@users.noreply.github.com>
2021-10-06 00:44:51 +0300

    example if-else

---
+++ variables/src/variables/Main.java
@@ -0,0 +1,21 @@
+package variables;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int number1 = 190;
+		int number2 = 227;
+		int number3 = 87;
+		int bıggestNumber = number1;
+		
+		if(bıggestNumber<number2) {
+			System.out.println("bıggest number is number2");
+		}else if(bıggestNumber<number3) {
+			System.out.println("bıggest number is number3");
+		}else{
+			System.out.println("bıggest number is number1");
+		}
+		
+
+	}
+}
